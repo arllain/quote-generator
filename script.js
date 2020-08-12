56d2ffd3707574f80bde968d538864cff7d959f0
Arllain <arllain@gmail.com>
2020-08-11 21:08:10 -0300

    Fetching Data With JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Get Quote from API
+async function getQuote() {
+  const proxyUrl = 'http://cors-anywhere.herokuapp.com/';
+  const apiURL =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
+  try {
+    const response = await fetch(proxyUrl + apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    getQuote();
+    console.log('no quote', error);
+  }
+}
+
+// On Load
+getQuote();
